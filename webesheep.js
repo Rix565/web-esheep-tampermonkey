d93767fe6bb584a1f1f751c98f0150c10c8ae590
Rix565 <61545240+Rix565@users.noreply.github.com>
2020-08-10 16:13:56 +0200

    created script ;)))

---
+++ webesheep.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         web eSheep
+// @namespace    http://esheep.petrucci.ch/
+// @version      0.4
+// @description  Add a sheep on your Google and Bing page!
+// @author       Adriano, modified by Rix56.
+// @include      *
+// @icon         http://esheep.petrucci.ch/favicon.gif
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.addEventListener("load", function() {
+      var s = document.createElement("script");
+      s.addEventListener("load", function() {
+          var sheep = new eSheep(); 
+          sheep.Start(); 
+      });
+      s.setAttribute("src", "https://adrianotiger.github.io/web-esheep/dist/esheep.min.js");
+      document.getElementsByTagName('head')[0].appendChild(s);
+    });
+})();
